888df695434e27b2ae759ca1bb3cb6ffbc57eb21
DatDonathan <jojo08082003@gmail.com>
2022-06-24 12:15:37 +0200

    Added visualization tools

---
+++ index.js
@@ -0,0 +1,38 @@
+class VisualizationLayer {
+
+    constructor() {
+        
+    }
+
+    init(seed) {
+
+    }
+
+    draw(x, z) {
+        return '#000000'
+    }
+
+}
+
+class VizualizationDrawer {
+
+    constructor() {
+        layers = [];
+    }
+
+    draw(seed, canvas) {
+        const ctx = canvas.getContext("2d")
+        //Init layers
+        for (const layer of this.layers) {
+            layer.init(seed)
+            //Draw
+            for (let x = 0; x < canvas.width; x++) {
+                for (let z = 0; z < canvas.height; z++) {
+                    ctx.fillStyle = layer.draw(x, z);
+                    ctx.fillRect(x, z, 1, 1);
+                }
+            }
+        }
+    }
+
+}
